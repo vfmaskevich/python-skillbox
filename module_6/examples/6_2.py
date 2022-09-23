a1c5896b3ee23b6f3a901e9cba361caf22243f98
Vladimir Maskevich <vfmaskevich@gmail.com>
2022-09-23 10:43:11 +0200

    6.2 Оператор while

---
+++ module_6/examples/6_2.py
@@ -0,0 +1,5 @@
+password = int(input('Введите пароль: '))
+while password != 235:
+    print('Неверный пароль!')
+    password = int(input('Попробуйте еще раз: '))
+print('Пароль верный. Добро пожаловать!')
