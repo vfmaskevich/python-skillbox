1c7698a28626a7795952e06b7c4ad7acae42d4c3
Vladimir Maskevich <vfmaskevich@gmail.com>
2022-09-23 10:52:33 +0200

    6.2 Оператор while. Пример задача "Шопинг".

--- module_6/examples/6_2.py
+++ module_6/examples/6_2.py
@@ -1,5 +1,12 @@
-password = int(input('Введите пароль: '))
-while password != 235:
-    print('Неверный пароль!')
-    password = int(input('Попробуйте еще раз: '))
-print('Пароль верный. Добро пожаловать!')
+# password = int(input('Введите пароль: '))
+# while password != 235:
+#     print('Неверный пароль!')
+#     password = int(input('Попробуйте еще раз: '))
+# print('Пароль верный. Добро пожаловать!')
+
+balance = int(input('Сколько денег пришло? '))
+while balance > 5000:
+    product_cost = int(input('Введите стоимость товара: '))
+    balance -= product_cost
+print('Внимание! На карте осталось мало денег! Остановитесь!')
+print('Баланс счета:', balance)
